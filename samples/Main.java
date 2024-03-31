696fccce2a8c5f25cac2694a1f8b23ec076eb16d
edilson258 <dev.258.edilson@gmail.com>
2024-03-31 19:53:28 +0200

    feat: parse methods

--- samples/Main.java
+++ samples/Main.java
@@ -2,4 +2,9 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
   }
+
+  private static int sum(int x, int y) {
+    int res = x + y;
+    return res;
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Borrow, fs::File, io::Read, process::exit};
+use std::{fs::File, io::Read, ops::BitAnd, process::exit, usize};
 
 const CONSTANT_CLASS: u8 = 7;
 const CONSTANT_FIELDREF: u8 = 9;
@@ -15,11 +15,51 @@ const CONSTANT_METHODHANDLE: u8 = 15;
 const CONSTANT_METHODTYPE: u8 = 16;
 const CONSTANT_INVOKEDYNAMIC: u8 = 18;
 
+const CLASS_ACCESS_FLAGS: [(&str, u16); 8] = [
+    ("ACC_PUBLIC", 0x0001),
+    ("ACC_FINAL", 0x0010),
+    ("ACC_SUPER", 0x0020),
+    ("ACC_INTERFACE", 0x0200),
+    ("ACC_ABSTRACT", 0x0400),
+    ("ACC_SYNTHETIC", 0x1000),
+    ("ACC_ANNOTATION", 0x2000),
+    ("ACC_ENUM", 0x4000),
+];
+
+const METHOD_ACCESS_FLAGS: [(&str, u16); 12] = [
+    ("ACC_PUBLIC", 0x0001),
+    ("ACC_PRIVATE", 0x0002),
+    ("ACC_PROTECTED", 0x0004),
+    ("ACC_STATIC", 0x0008),
+    ("ACC_FINAL", 0x0010),
+    ("ACC_SYNCHRONIZED", 0x0020),
+    ("ACC_BRIDGE", 0x0040),
+    ("ACC_VARARGS", 0x0080),
+    ("ACC_NATIVE", 0x0100),
+    ("ACC_ABSTRACT", 0x0400),
+    ("ACC_STRICT", 0x0800),
+    ("ACC_SYNTHETIC", 0x1000),
+];
+
 fn main() {
     let file_path = "samples/Main.class";
     let content = read_file2bytes(file_path);
     let mut parser = ParseClass::new(&content);
     parser.parse();
+}
+
+#[derive(Debug)]
+struct MethodAttr {
+    name: String,
+    bytes: Vec<u8>,
+}
+
+#[derive(Debug)]
+struct Method {
+    access_flags: Vec<String>,
+    name: String,
+    descriptor: String,
+    attrs: Vec<MethodAttr>,
 }
 
 #[derive(Debug)]
@@ -41,6 +81,10 @@ struct JavaClassFile {
     minor: u16,
     major: u16,
     constant_pool: ConstantPool,
+    access_flags: Vec<String>,
+    this_class: String,
+    super_class: String,
+    methods: Vec<Method>,
 }
 
 impl JavaClassFile {
@@ -49,6 +93,12 @@ impl JavaClassFile {
         println!("Minor Version: 0x{:x}", self.minor);
         println!("Major Version: 0x{:x}", self.major);
         println!("{:#?}", self.constant_pool);
+        println!("Access Flags: {:?}", self.access_flags);
+        println!(
+            "This Class : {:?}, Super Class: {:?}",
+            self.this_class, self.super_class
+        );
+        println!("{:?}", self.methods);
     }
 }
 
@@ -67,7 +117,89 @@ impl<'a> ParseClass<'a> {
         class.minor = self.parse_u2();
         class.major = self.parse_u2();
         class.constant_pool = self.parse_constant_pool();
+        let access_flag_mask = self.parse_u2();
+        class.access_flags = self.parse_access_flags(access_flag_mask, &CLASS_ACCESS_FLAGS);
+
+        let this_class_index = self.parse_u2();
+        let super_class_index = self.parse_u2();
+        class.this_class = self
+            .constant_pool_query(&class.constant_pool, this_class_index as usize)
+            .unwrap();
+        class.super_class = self
+            .constant_pool_query(&class.constant_pool, 1 + super_class_index as usize)
+            .unwrap();
+
+        let interfaces_count = self.parse_u2();
+        for _ in 0..interfaces_count {
+            assert!(
+                false,
+                "[ERROR]:{}:{}: Interface parser not implemented yet\n",
+                file!(),
+                line!()
+            );
+        }
+
+        let fields_count = self.parse_u2();
+        for _ in 0..fields_count {
+            assert!(
+                false,
+                "[ERROR]:{}:{}: Field parser not implemented yet\n",
+                file!(),
+                line!()
+            );
+        }
+
+        class.methods = self.parse_methods(&class.constant_pool);
+
         class.dump();
+    }
+
+    fn parse_methods(&mut self, cp: &ConstantPool) -> Vec<Method> {
+        let mut methods: Vec<Method> = vec![];
+
+        for _ in 0..self.parse_u2() {
+            let mask = self.parse_u2();
+            let name_index = self.parse_u2();
+            let descriptor_index = self.parse_u2();
+
+            methods.push(Method {
+                access_flags: self.parse_access_flags(mask, &METHOD_ACCESS_FLAGS),
+                name: self
+                    .constant_pool_query(cp, (name_index - 1) as usize)
+                    .unwrap(),
+                descriptor: self
+                    .constant_pool_query(cp, (descriptor_index - 1) as usize)
+                    .unwrap(),
+                attrs: self.parse_method_attrs(cp),
+            });
+        }
+        methods
+    }
+
+    fn parse_method_attrs(&mut self, cp: &ConstantPool) -> Vec<MethodAttr> {
+        let mut attrs: Vec<MethodAttr> = vec![];
+        for _ in 0..self.parse_u2() {
+            let name_index = self.parse_u2();
+            let length = self.parse_u4();
+
+            attrs.push(MethodAttr {
+                name: self
+                    .constant_pool_query(cp, (name_index - 1) as usize)
+                    .unwrap(),
+                bytes: self.parse_n(length as usize).to_vec(),
+            });
+        }
+        attrs
+    }
+
+    pub fn parse_access_flags(&self, mask: u16, access_flags: &[(&str, u16)]) -> Vec<String> {
+        let mut flags: Vec<String> = vec![];
+        for (name, value) in access_flags {
+            if mask.bitand(value) != 0 {
+                flags.push(name.to_string());
+            }
+        }
+        flags
     }
 
     pub fn parse_constant_pool(&mut self) -> ConstantPool {
@@ -125,7 +257,7 @@ impl<'a> ParseClass<'a> {
                     tag,
                     tag_name: "CONSTANT_STRING".to_string(),
                     entries: vec![("string_index".to_string(), self.parse_u2())],
-                    bytes: None
+                    bytes: None,
                 }),
                 _ => {
                     eprintln!("[ERROR]:{}:{}: Unknown tag {}", file!(), line!(), tag);
@@ -135,6 +267,10 @@ impl<'a> ParseClass<'a> {
         }
 
         constant_pool
+    }
+
+    fn constant_pool_query(&self, pool: &ConstantPool, index: usize) -> Option<String> {
+        pool.info.get(index).unwrap().bytes.clone()
     }
 
     fn parse_u1(&mut self) -> u8 {
